0fceb97f006f1a85e1fbd1756ccd90430cdea4c0
thongnguyenit99 <nvthong.it@gmail.com>
2020-09-27 20:46:59 +0700

    create module:HelloWorld

---
+++ app/code/thongnv/HelloWorld/Controller/HomePage/default.php
@@ -0,0 +1,9 @@
+<?php
+namespace  thongnv\HelloWorld\Controller\HomePage;
+
+class  Index  \Magento\Framework\App\Action\Action{
+    public function execute(){
+         echo "Hello World Thongnv";
+    }
+}
+?>
